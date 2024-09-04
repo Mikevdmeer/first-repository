18788015e773d5a553df40028962bab027a44100
Mikevdmeer <99081100@mydavinci.nl>
2024-09-04 14:02:15 +0200

    commit

---
+++ Bootcamp/Untitled-4.py
@@ -0,0 +1,10 @@
+getal1 = int(input("Voer getal 1 in: "))
+getal2 = int(input("Voer getal 2 in: "))
+getal3 = int(input("Voer getal 3 in: "))
+getal4 = int(input("Voer getal 4 in: "))
+getal5 = int(input("Voer getal 5 in: "))
+getal6 = int(input("Voer getal 6 in: "))
+
+getallen = [getal1, getal2, getal3, getal4, getal5, getal6]
+getallen.sort()
+print(getallen)
---
+++ Bootcamp/Untitled-5.py
@@ -0,0 +1,5 @@
+fruit = ["Appel", "Banaan", "Ananas", "Mandarijn", "Sinaasappel", "Watermeloen"]
+keuze = input("Kies een fruit om het te verwijderen: ")
+fruit.remove(keuze)
+print(keuze, "is verwijderd")
+print(fruit)
