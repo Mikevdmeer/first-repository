b34aef4d90675ac726a4b61db7741836f0cd77c4
Mikevdmeer <99081100@mydavinci.nl>
2024-09-04 09:02:23 +0200

    commit

--- Bootcamp/bc_l3_opdracht1.py
+++ Bootcamp/bc_l3_opdracht1.py
@@ -1,3 +1,14 @@
+
+
+
+var1 = float(input("Voer het eerste getal in: "))
+var2 = float(input("Voer het tweede getal in: "))
+var3 = float(input("Voer het derde getal in: "))
+
+gemiddelde = (var1 + var2 + var3) / 3
+
+print(f'Het gemiddelde is: {gemiddelde}')
+
 description = 'jaar'
 
 years = 1
@@ -8,3 +19,4 @@ minutes = hours * 60
 seconds = minutes * 60
 
 print(f'In een {description} zitten {seconds} seconden')
+
