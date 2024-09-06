ffb0383e3e5dae88d3b9b93cf27841f3c51f1ae4
Mikevdmeer <99081100@mydavinci.nl>
2024-09-06 08:54:47 +0200

    commit

---
+++ Bootcamp/Untitled-7.py
@@ -0,0 +1,6 @@
+leeftijd = int(input("Hoe oud bent u?: "))
+
+if not leeftijd >=18:
+    print("Geen toegang!")
+else:
+    print("toegang!")
---
+++ Bootcamp/Untitled-8.py
@@ -0,0 +1,8 @@
+leeftijd = 18
+snor = "j"
+diploma = "j"
+
+if leeftijd >=18 and snor == "j" or leeftijd <=18 and diploma == "j":
+    print("aangenomen!")
+else:
+    print("niet aangenomen!")
